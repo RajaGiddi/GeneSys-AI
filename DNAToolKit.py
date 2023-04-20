facbdb0779e3e07f3d0c58905462f9832c1aa5ab
Orion <97639175+RajaGiddi@users.noreply.github.com>
2023-04-20 23:13:22 +0000

    Used GPT to combine the 2 GC functions cause I felt lazy

--- DNAToolKit.py
+++ DNAToolKit.py
@@ -66,23 +66,40 @@ def reverseComplementary(dna):
 
 # Computes the GC content of a given DNA sequence
 
-
-def GC_Content(dna):
-    total_GC = dna.count('G') + dna.count('C')
-    GC_percentage = (total_GC / len(dna)) * 100
-    return GC_percentage
+#def GC_Content(dna):
+#    total_GC = dna.count('G') + dna.count('C')
+#    GC_percentage = (total_GC / len(dna)) * 100
+#    return GC_percentage
 
 
 # k = the number of nucleotides for each subsection
-def GC_Content_subsections(dna, k=5):
+#def GC_Content_subsections(dna, k=5):
     
-    subsections = []
-    for i in range(0, len(dna) - k + 1, k):
-        subsections.append(dna[i:i+k])
+#    subsections = []
+#    for i in range(0, len(dna) - k + 1, k):
+#        subsections.append(dna[i:i+k])
     
-    for i in range(len(subsections)):
-        total_GC = subsections[i].count('G') + subsections[i].count('C')
-        GC_percentage = 100 * (total_GC / len(subsections[i]))
-        result = print(f"Subsection: {subsections[i]} - GC Content: {GC_percentage}")
+#    for i in range(len(subsections)):
+#        total_GC = subsections[i].count('G') + subsections[i].count('C')
+#        GC_percentage = 100 * (total_GC / len(subsections[i]))
+#        result = print(f"Subsection: {subsections[i]} - GC Content: {GC_percentage}")
+
+#    return result
+
+# The combined above two functions into one GC content calculator
+def GC_Content_combined(dna, k=None):
+    if k is None:
+        total_GC = dna.count('G') + dna.count('C')
+        GC_percentage = (total_GC / len(dna)) * 100
+        result = print(f"GC Content: {GC_percentage}")
+    else:
+        subsections = []
+        for i in range(0, len(dna) - k + 1, k):
+            subsections.append(dna[i:i+k])
+
+        for i in range(len(subsections)):
+            total_GC = subsections[i].count('G') + subsections[i].count('C')
+            GC_percentage = 100 * (total_GC / len(subsections[i]))
+            result = print(f"Subsection: {subsections[i]} - GC Content: {GC_percentage}")
 
     return result
