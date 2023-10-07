e8e4d7adbd38c7239188f8da25cf20f7c214ab4a
Raja Giddi <97639175+RajaGiddi@users.noreply.github.com>
2023-10-07 02:39:46 -0400

    no longer need the codon table, comp nucs, or nucs lists and dicts

--- genesys/DNAToolKit.py
+++ genesys/DNAToolKit.py
@@ -10,47 +10,6 @@ from Bio import AlignIO
 from io import StringIO
 import matplotlib.pyplot as plt
 
-# Counting DNA Nucleotides
-
-nucleotides = ['A', 'C', 'T', 'G']
-complementary_nucleotides = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
-
-codon_table = {
-    "UUU": "F", "UUC": "F", "UUA": "L", "UUG": "L", "CUU": "L", "CUC": "L",
-    "CUA": "L", "CUG": "L", "AUU": "I", "AUC": "I", "AUA": "I", "AUG": "M",
-    "GUU": "V", "GUC": "V", "GUA": "V", "GUG": "V", "UCU": "S", "UCC": "S",
-    "UCA": "S", "UCG": "S", "CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P",
-    "ACU": "T", "ACC": "T", "ACA": "T", "ACG": "T", "GCU": "A", "GCC": "A",
-    "GCA": "A", "GCG": "A", "UAU": "Y", "UAC": "Y", "UAA": "*", "UAG": "*",
-    "CAU": "H", "CAC": "H", "CAA": "Q", "CAG": "Q", "AAU": "N", "AAC": "N",
-    "AAA": "K", "AAG": "K", "GAU": "D", "GAC": "D", "GAA": "E", "GAG": "E",
-    "UGU": "C", "UGC": "C", "UGA": "*", "UGG": "W", "CGU": "R", "CGC": "R",
-    "CGA": "R", "CGG": "R", "AGU": "S", "AGC": "S", "AGA": "R", "AGG": "R",
-    "GGU": "G", "GGC": "G", "GGA": "G", "GGG": "G"
-}
-
-aa_protein_mass = {'A': 71.03711,
-                   'C': 103.00919,
-                   'D': 115.02694,
-                   'E': 129.04259,
-                   'F': 147.06841,
-                   'G': 57.02146,
-                   'H': 137.05891,
-                   'I': 113.08406,
-                   'K': 128.09496,
-                   'L': 113.08406,
-                   'M': 131.04049,
-                   'N': 114.04293,
-                   'P': 97.05276,
-                   'Q': 128.05858,
-                   'R': 156.10111,
-                   'S': 87.03203,
-                   'T': 101.04768,
-                   'V': 99.06841,
-                   'W': 186.07931,
-                   'Y': 163.06333}
-
-
 def sequence_type(filepath):
     """ Determine the type of sequence in a FASTA file.
 
