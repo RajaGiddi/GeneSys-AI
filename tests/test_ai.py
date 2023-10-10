d720114f7de9f5ca95e6f85778ae551bf87ecd79
Raja Giddi <97639175+RajaGiddi@users.noreply.github.com>
2023-10-10 01:35:13 -0400

    added some tests for each function

--- tests/test_ai.py
+++ tests/test_ai.py
@@ -50,9 +50,73 @@ def test_ask_count_nucleotides():
 
 def test_ask_multiple_sequence_alignment():
     response = question_about_file(
-        "What is the multiple sequence alignment of the sequences?",
+        "Perform MSA on the given sequence?",
         os.path.join(TEST_DATA_DIR, "msa.fasta")
     )
     response_message = response["choices"][0]["message"]
     assert response_message.get("function_call")
     assert response_message["function_call"]["name"] == "multiple_sequence_alignment"
+
+
+def test_ask_transcription():
+    response = question_about_file(
+        "What is the mRNA transcript of the given seq?",
+        os.path.join(TEST_DATA_DIR, "sequence.fasta")
+    )
+    response_message = response["choices"][0]["message"]
+    assert response_message.get("function_call")
+    assert response_message["function_call"]["name"] == "transcription"
+
+def test_ask_translation():
+    response = question_about_file(
+        "What is the amino acid sequence?",
+        os.path.join(TEST_DATA_DIR, "sequence.fasta")
+    )
+    response_message = response["choices"][0]["message"]
+    assert response_message.get("function_call")
+    assert response_message["function_call"]["name"] == "translation"
+
+def test_ask_iso_electric_point():
+    response = question_about_file(
+        "What is the isoelectric point of the given protein sequence?",
+        os.path.join(TEST_DATA_DIR, "sequence.fasta")
+    )
+    response_message = response["choices"][0]["message"]
+    assert response_message.get("function_call")
+    assert response_message["function_call"]["name"] == "isoelectric_point"
+
+def test_ask_mass_calculator():
+    response = question_about_file(
+        "What is the molecular weight of the given protein sequence?",
+        os.path.join(TEST_DATA_DIR, "sequence.fasta")
+    )
+    response_message = response["choices"][0]["message"]
+    assert response_message.get("function_call")
+    assert response_message["function_call"]["name"] == "mass_calculator"
+
+def test_ask_restriction_enzyme():
+    response = question_about_file(
+        "What are the restriction sites in the given sequence?",
+        os.path.join(TEST_DATA_DIR, "sequence.fasta")
+    )
+    response_message = response["choices"][0]["message"]
+    assert response_message.get("function_call")
+    assert response_message["function_call"]["name"] == "restriction_sites"
+
+def test_ask_gc_content():
+    response = question_about_file(
+        "What is the GC content of the third sequence?",
+        os.path.join(TEST_DATA_DIR, "sequence.fasta")
+    )
+    response_message = response["choices"][0]["message"]
+    assert response_message.get("function_call")
+    assert response_message["function_call"]["name"] == "gc_content"
+
+def test_ask_orf():
+    response = question_about_file(
+        "What are the open reading frames in the given sequence?",
+        os.path.join(TEST_DATA_DIR, "random_dna.fasta")
+    )
+    response_message = response["choices"][0]["message"]
+    assert response_message.get("function_call")
+    assert response_message["function_call"]["name"] == "open_reading_frames"
