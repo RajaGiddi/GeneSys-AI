56181abf752f7011578e1563190403ada7005f19
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-11-20 13:09:54 -0500

    test(utils): functions without annotated metadata

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -6,6 +6,9 @@ from typing_extensions import Doc
 
 from genesys import utils
 
+def f(x: float, y: float) -> float:
+    """A simple function without documentation in annotated metadata."""
+    return x + y
 
 def pythagoras(
     a: Annotated[float, Doc("The length of side A.")],
@@ -30,7 +33,7 @@ def test_annotated_param():
     sign = inspect.signature(pythagoras)
     assert utils.is_annotated(sign.parameters["a"].annotation)
 
-def test_get_function_params():
+def test_get_param_metadata():
     schema = utils.gen_function_schema(pythagoras)
     assert schema["parameters"]["properties"]["a"]["type"] == "number"
     assert schema["parameters"]["properties"]["b"]["type"] == "number"
@@ -42,3 +45,20 @@ def test_get_function_params():
         schema["parameters"]["properties"]["b"]["description"]
         == "The length of side B."
     )
+
+def test_get_simple_function_schema():
+    schema = utils.gen_function_schema(f)
+    assert schema["name"] == "f"
+    assert (
+        schema["description"]
+        == "A simple function without documentation in annotated metadata."
+    )
+
+
+def test_get_simple_param_annotations():
+    schema = utils.gen_function_schema(f)
+    assert schema["parameters"]["properties"]["x"]["type"] == "number"
+    assert schema["parameters"]["properties"]["y"]["type"] == "number"
+    assert schema["parameters"]["properties"]["x"].get("description") is None
+    assert schema["parameters"]["properties"]["y"].get("description") is None
+
