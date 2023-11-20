f18d59f9b145bb3c7d646c330aad40a67a672c9a
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-11-20 12:22:26 -0500

    impr(utils): fn to check if type has metadata
    Makes `test_annotated_param` pass.

--- genesys/utils.py
+++ genesys/utils.py
@@ -1,6 +1,25 @@
 import inspect
 from typing import Callable, Any
 
+def _lenient_issubclass(cls: Any, class_or_tuple: Any) -> bool:
+    """
+    Adapted from pydantic._internal._utils.lenient_issubclass().
+    """
+    try:
+        return isinstance(cls, type) and issubclass(cls, class_or_tuple)
+    except TypeError:
+        if isinstance(cls, (typing._GenericAlias, types.GenericAlias, types.UnionType)):
+            return False
+        else:
+            raise
+
+def is_annotated(ann_type: Any) -> bool:
+    """
+    Adapted from pydantic._internal._typing_extras.is_annotated().
+    """
+    origin = typing.get_origin(ann_type)
+    return origin is not None and _lenient_issubclass(origin, Annotated)
+
 
 def gen_function_schema(function: Callable[..., Any]) -> dict[str, Any]:
     schema = {
