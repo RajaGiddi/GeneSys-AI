0eec36a49c45875d4f579d434ecdd5055609bb96
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2023-11-20 12:26:05 -0500

    impr(utils): implement fn to gen JSON schema
    Used to specify function calling tools for assistants.

--- genesys/utils.py
+++ genesys/utils.py
@@ -1,5 +1,10 @@
 import inspect
-from typing import Callable, Any
+import types
+import typing
+from typing import Annotated, Any, Callable
+
+from typing_extensions import Doc
+
 
 def _lenient_issubclass(cls: Any, class_or_tuple: Any) -> bool:
     """
@@ -20,14 +25,39 @@ def is_annotated(ann_type: Any) -> bool:
     origin = typing.get_origin(ann_type)
     return origin is not None and _lenient_issubclass(origin, Annotated)
 
+def to_json_type(tp: type) -> str:
+    if tp == bool:
+        return 'boolean'
+    elif tp == float or tp == int:
+        return 'number'
+    elif tp == type:
+        return 'string'
+    elif tp == list:
+        return 'array'
+    elif tp == dict:
+        return 'object'
+    else:
+        return 'null'
 
-def gen_function_schema(function: Callable[..., Any]) -> dict[str, Any]:
+def gen_function_schema(func: Callable[..., Any]) -> dict[str, Any]:
+    props = {}
+
+    for name, param in inspect.signature(func).parameters.items():
+        type_hint = param.annotation
+        props[name] = {}
+        if is_annotated(type_hint):
+            props[name]["type"] = to_json_type(typing._strip_annotations(type_hint))
+            for metadata in type_hint.__metadata__:
+                if isinstance(metadata, Doc):
+                    props[name]["description"] = metadata.documentation
+
     schema = {
-        "name": function.__name__,
-        "description": inspect.getdoc(function),
+        "name": func.__name__,
+        "description": inspect.getdoc(func),
         "parameters": {
             "type": "object",
-            "properties": {},
+            "properties": props,
         }
     }
+
     return schema
