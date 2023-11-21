899f663c3e0ac38798335067741f72fa8e05e054
Raja Giddi <97639175+RajaGiddi@users.noreply.github.com>
2023-11-20 21:49:56 -0500

    Refactor create_assistant_and_run method to include a toolkit parameter

--- genesys/assistants/ra.py
+++ genesys/assistants/ra.py
@@ -2,8 +2,7 @@ from openai import OpenAI
 import time
 import json
 from genesys.env import load_dotenv
-import genesys.tools.pubmed as pubmed_tools
-import inspect
+
 
 load_dotenv()
 
@@ -12,7 +11,7 @@ class ResearchAssistant:
         self.client = OpenAI(api_key=api_key)
         self.model = model
 
-    def create_assistant_and_run(self, user_input, assistant_name, instructions, tools_list, thread_instructions, file_path=None):
+    def create_assistant_and_run(self, user_input, assistant_name, instructions, tools_list, thread_instructions, toolkit, file_path=None):
         assistant = self.client.beta.assistants.create(
             name=assistant_name,
             instructions=instructions,
@@ -22,9 +21,9 @@ class ResearchAssistant:
         thread = self.client.beta.threads.create()
         self.client.beta.threads.messages.create(thread_id=thread.id, role="user", content=user_input)
         run = self.client.beta.threads.runs.create(thread_id=thread.id, assistant_id=assistant.id, instructions=thread_instructions)
-        self.check_run_status(thread.id, run.id)
+        self.check_run_status(thread.id, run.id, toolkit)
 
-    def check_run_status(self, thread_id, run_id):
+    def check_run_status(self, thread_id, run_id, toolkit):
         while True:
             time.sleep(5)
             run_status = self.client.beta.threads.runs.retrieve(thread_id=thread_id, run_id=run_id)
@@ -37,35 +36,37 @@ class ResearchAssistant:
                     print(f"{role.capitalize()}: {content}")
                 break
             elif run_status.status == 'requires_action':
-                self.process_required_actions(thread_id, run_id, run_status.required_action.submit_tool_outputs.model_dump())
+                self.process_required_actions(thread_id, run_id, run_status.required_action.submit_tool_outputs.model_dump(), toolkit)
             else:
                 print("Waiting for the Assistant to process...")
-        
-    def build_function_map(self):
-        function_map = {}
-        for name, func in inspect.getmembers(pubmed_tools, inspect.isfunction):
-            function_map[name] = func
-        return function_map
 
-
-    def process_required_actions(self, thread_id, run_id, required_actions):
+    def process_required_actions(self, thread_id, run_id, required_actions, toolkit):
         tool_outputs = []
-        function_map = self.build_function_map()
 
         for action in required_actions["tool_calls"]:
             func_name = action['function']['name']
             arguments = json.loads(action['function']['arguments'])
 
-            if func_name in function_map:
-                function_to_call = function_map[func_name]
+            try:
+                # Dynamically get the function from the specified toolkit
+                function_to_call = getattr(toolkit, func_name)
+                
+
+                # Call the function with unpacked arguments
                 output = function_to_call(**arguments)
+
+                # Convert the output to a string and append it to tool_outputs
                 output_str = json.dumps(output)
                 tool_outputs.append({
                     "tool_call_id": action['id'],
                     "output": output_str
                 })
-            else:
-                raise ValueError(f"Unknown function: {func_name}")
 
+            except AttributeError:
+                raise ValueError(f"Unknown function: {func_name}")
+            except json.JSONDecodeError:
+                raise ValueError("Error decoding arguments for the function call")
+
+        # Submitting outputs back to the Assistant
         print("Submitting outputs back to the Assistant...")
         self.client.beta.threads.runs.submit_tool_outputs(thread_id=thread_id, run_id=run_id, tool_outputs=tool_outputs)
