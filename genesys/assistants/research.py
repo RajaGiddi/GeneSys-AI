3f1ba81daa6fcaad9c658085f26a73590b3c2534
Raja Giddi <97639175+RajaGiddi@users.noreply.github.com>
2023-11-20 21:49:43 -0500

    Add pubmed_tools to research assistant

--- genesys/assistants/research.py
+++ genesys/assistants/research.py
@@ -1,4 +1,5 @@
 from genesys.assistants.ra import *
+import genesys.tools.pubmed as pubmed_tools
 import os
 
 literature_tools = [
@@ -30,4 +31,6 @@ assistant = ResearchAssistant(api_key=os.environ['OPENAI_API_KEY'])
 
 user_input_medical = "What are the latest advancements in neurology?"
 assistant.create_assistant_and_run(user_input=user_input_medical, assistant_name="Borj",
-                                   instructions="Find papers most relevant to my query.", tools_list=literature_tools, thread_instructions="Your output of papers should always be bullet points where the main bullet point is the paper and the sub-bullet points are the abstract and link to the paper.")
+                                   instructions="Find papers most relevant to my query.",
+                                   tools_list=literature_tools, thread_instructions="Your output of papers should always be bullet points where the main bullet point is the paper and the sub-bullet points are the abstract and link to the paper.",
+                                   toolkit=pubmed_tools)
