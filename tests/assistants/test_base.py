f7385fee8ee0b9613362acddd627dca4a1c63c24
Raja Giddi <97639175+RajaGiddi@users.noreply.github.com>
2023-11-22 00:25:25 -0500

    Add BaseAssistant class to test_base.py
    Co-authored-by: Sandeep Dilip <sanman1k98@users.noreply.github.com>

--- tests/assistants/test_base.py
+++ tests/assistants/test_base.py
@@ -1,8 +1,17 @@
-from genesys.assistants.base import create_base_assistant
+from genesys.assistants.base import BaseAssistant, create_base_assistant
 from genesys.openai import openai_client
 
 def test_create_assistant():
     test_assistant = create_base_assistant(
+        name="Test Assistant",
+        description="Used for testing.",
+        metadata={"test": True}
+    )
+    also_test_assistant = openai_client.beta.assistants.retrieve(test_assistant.id)
+    assert test_assistant.id == also_test_assistant.id
+
+def test_base_assistant_class():
+    test_assistant = BaseAssistant(
         name="Test Assistant",
         description="Used for testing.",
         metadata={"test": True}
