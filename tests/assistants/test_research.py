3cfcbb28c30f02a39b8bf469a5e228b84e1556e7
Raja Giddi <97639175+RajaGiddi@users.noreply.github.com>
2023-11-22 00:25:29 -0500

    Add test for research assistant conversation
    Co-authored-by: Sandeep Dilip <sanman1k98@users.noreply.github.com>

---
+++ tests/assistants/test_research.py
@@ -0,0 +1,32 @@
+import pytest
+import time
+import json
+from genesys.openai import openai_client as client
+from genesys.assistants import research_assistant
+
+def test_assistant_conversation():
+    run = client.beta.threads.create_and_run(
+        assistant_id=research_assistant.id,
+        thread = {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": "Can you get me papers about FMR1?",
+                }
+            ]
+        }
+    )
+    
+    while True:
+        time.sleep(1)
+        run = client.beta.threads.runs.retrieve(run_id=run.id, thread_id=run.thread_id)
+        
+        if run.status == "requires_action" and run.required_action is not None:
+            tool_outputs = research_assistant.get_tool_outputs(run)
+            res = client.beta.threads.runs.submit_tool_outputs(run_id=run.id, thread_id=run.thread_id, tool_outputs=tool_outputs)
+        elif run.status == "completed":
+            break
+
+    messages = client.beta.threads.messages.list(thread_id=run.thread_id)
+    print(messages.model_dump_json(indent=2))
+    assert run.status == "completed"
