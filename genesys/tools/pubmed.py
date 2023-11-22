a99284b54819252c435f35f74c60d40d7e08cc7e
Raja Giddi <97639175+RajaGiddi@users.noreply.github.com>
2023-11-21 22:12:47 -0500

    Refactor fetch_papers function and add type annotations
    Co-Authored: Sandeep Dilip

--- genesys/tools/pubmed.py
+++ genesys/tools/pubmed.py
@@ -1,19 +1,42 @@
+from typing import Annotated
+from typing_extensions import Doc
 import requests
 import xml.etree.ElementTree as ET
 
 base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
 
-def parse_search_results(search_result):
+
+def _parse_search_results(
+    search_result: Annotated[
+        str,
+        Doc("Represents raw XML data in a string format")
+    ]
+) -> list[str]:
+    """
+    Parses the XML response from PubMed's ESearch API and returns a list of PMIDs.
+    """
     root = ET.fromstring(search_result)
     return [id_tag.text for id_tag in root.findall('.//IdList/Id')]
 
-def fetch_papers(query: str , max_results=10) -> list:
-    # Search for papers
+
+def fetch_papers(
+    query: Annotated[
+        str,
+        Doc("A string of search terms that best match the user's query")
+    ],
+    max_results: Annotated[
+        int,
+        Doc("Maximum number of papers returned")
+    ] = 10
+) -> list:
+    """
+    Fetches a list of papers from PubMed based on the given query.
+    """
+
     search_url = f"{base_url}esearch.fcgi?db=pubmed&term={query}&retmode=xml"
     search_response = requests.get(search_url)
-    id_list = parse_search_results(search_response.text)
+    id_list = _parse_search_results(search_response.text)
 
-    # Fetch paper details
     ids = ','.join(id_list[:max_results])
     fetch_url = f"{base_url}efetch.fcgi?db=pubmed&id={ids}&retmode=xml"
     fetch_response = requests.get(fetch_url)
@@ -24,5 +47,5 @@ def fetch_papers(query: str , max_results=10) -> list:
         pmid = article.find('.//PMID').text
         title = article.find('.//ArticleTitle').text
         papers.append((pmid, title))
-    
+
     return papers
