c428996decc605d3cbf5e325e21f577593eb24bc
Raja Giddi <97639175+RajaGiddi@users.noreply.github.com>
2023-11-24 23:17:41 -0500

    Add sequence module to Research Assistant
    Co-authored-by: Sandeep Dilip <sanman1k98@users.noreply.github.com>

--- genesys/assistants/__init__.py
+++ genesys/assistants/__init__.py
@@ -1,5 +1,5 @@
 from .base import BaseAssistant
-from ..tools import pubmed
+from ..tools import pubmed, sequence
 from ..utils import gen_tools_schema
 
 
@@ -9,14 +9,14 @@ research_assistant = BaseAssistant(
     tools_module=pubmed,
     name="Research Assistant",
     description="A research assistant to help you with your work.",
-    instructions="Help find sources for my research paper.",
+    instructions="Help find papers based on query. You should always return ",
     tools=gen_tools_schema(pubmed),
 )
 
-#bioinformatician_assistant = client.beta.assistants.create(
-#    model=DEFAULT_MODEL,
-#    name="Bioinformatician Assistant",
-#    description="A bioinformatician assistant to help you analyze your biological data.",
-#    instructions="Help me find the best way to analyze my data.",
-#    tools=gen_tools_schema(pubmed),
-#)
+bioinformatician_assistant = BaseAssistant(
+    tools_module=sequence,
+    name="Bioinformatician Assistant",
+    description="A bioinformatician assistant to help you analyze your biological data.",
+    instructions="Choose the right tools that best fit the user's query",
+    tools=gen_tools_schema(sequence),
+)
