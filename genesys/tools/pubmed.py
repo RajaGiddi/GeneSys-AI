e36a3bfe54e1f1f8d5ea6c2a8966827a421cb213
Raja Giddi <97639175+RajaGiddi@users.noreply.github.com>
2023-11-27 15:54:04 -0500

    Update fetch_papers function to include PMCID and DOI URLs

--- genesys/tools/pubmed.py
+++ genesys/tools/pubmed.py
@@ -6,6 +6,7 @@ from typing_extensions import Doc
 
 base_url = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
 
+
 def _parse_search_results(search_result: str) -> list[str]:
     """
     Parses the XML response from PubMed's ESearch API and returns a list of PMIDs.
@@ -13,11 +14,12 @@ def _parse_search_results(search_result: str) -> list[str]:
     root = ET.fromstring(search_result)
     return [id_tag.text for id_tag in root.findall('.//IdList/Id')]
 
+
 def fetch_papers(
-        query: Annotated[str, Doc("Search query to submit to PubMed.")],
-        max_results: int = 10
-    ):
-    """Search PubMed and get the title, abstract, and URL for each search result."""
+    query: Annotated[str, Doc("Search query to submit to PubMed.")],
+    max_results: int = 10
+):
+    """Search PubMed and get the title, abstract, URL, and PMCID for each search result."""
 
     search_url = f"{base_url}esearch.fcgi?db=pubmed&term={query}&retmode=xml"
     search_response = requests.get(search_url)
@@ -34,9 +36,24 @@ def fetch_papers(
         title = article.find('.//ArticleTitle').text
         abstract = article.find('.//Abstract/AbstractText').text
 
+        # Extracting PMCID
+        pmcid = None
+        doi = None
+        article_ids = article.findall('.//ArticleIdList/ArticleId')
+        for aid in article_ids:
+            if aid.get('IdType') == 'pmc':
+                pmcid = aid.text
+            elif aid.get('IdType') == 'doi':
+                doi = aid.text
+
+        pmid_url = f"https://pubmed.ncbi.nlm.nih.gov/{pmid}/"
+        pmcid_url = f"https://www.ncbi.nlm.nih.gov/pmc/articles/{pmcid}/"
+        doi_url = f"https://doi.org/{doi}"
+
         papers.append({
-            "pubmed_url": f"https://pubmed.ncbi.nlm.nih.gov/{pmid}/",
-            "pubmed_id": pmid,
+            "pmid_url": pmid_url,
+            "pmcid_url": pmcid_url,
+            "doi_url": doi_url,
             "title": title,
             "abstract": abstract,
         })
