6d9b6e13bb6c2f0e5425e426bd4bd989dbfdbfc5
Raja Giddi <97639175+RajaGiddi@users.noreply.github.com>
2023-11-26 20:39:17 -0500

    Update instructions for Research Assistant

--- genesys/assistants/__init__.py
+++ genesys/assistants/__init__.py
@@ -8,7 +8,7 @@ research_assistant = BaseAssistant(
     functions_module=pubmed,
     name="Research Assistant",
     description="A research assistant to help you with your work.",
-    instructions="Help find papers based on query.",
+    instructions="Help find papers based on query. You should always return in a bullet point format with the title , 2-3 'Key Points' based on the abstract, and URL for each search result.",
 )
 
 bioinformatician_assistant = BaseAssistant(
